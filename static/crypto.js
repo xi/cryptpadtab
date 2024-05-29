d291c2f5ff8a125d1ec58a73ff8242fe73f4a8ae
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-05-29 16:50:36 +0200

    combine everything into a single module

--- static/crypto.js
+++ static/crypto.js
@@ -1,3 +1,57 @@
+var b64table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+
+export function b64encode(bytes) {
+	var result = '';
+	var push = x => result += b64table.charAt(x & 0b111111);
+	for (let i = 0; i < bytes.length; i += 3) {
+		var b1 = bytes[i];
+		var b2 = bytes[i + 1];
+		var b3 = bytes[i + 2];
+
+		push(b1 >> 2);
+		push((b1 << 4) | (b2 >> 4));
+		if (!isNaN(b2)) push((b2 << 2) | (b3 >> 6));
+		if (!isNaN(b3)) push(b3);
+	}
+	return result;
+}
+
+export function b64decode(string) {
+	var result = [];
+	for (let i = 0; i < string.length; i += 4) {
+		var e1 = b64table.indexOf(string[i]);
+		var e2 = b64table.indexOf(string[i + 1]);
+		var e3 = b64table.indexOf(string[i + 2]);
+		var e4 = b64table.indexOf(string[i + 3]);
+
+		result.push((e1 << 2) | (e2 >> 4));
+		if (e3 !== -1) result.push((e2 << 4) | (e3 >> 2));
+		if (e4 !== -1) result.push((e3 << 6) | e4);
+	}
+	// will truncate the values automatically
+	return new Uint8Array(result);
+}
+
+async function applyStream(bytes, stream) {
+	var readableStream = new ReadableStream({
+		start(controller) {
+			controller.enqueue(bytes);
+			controller.close();
+		}
+	});
+	var resultStream = await readableStream.pipeThrough(stream);
+	var result = await new Response(resultStream).arrayBuffer();
+	return new Uint8Array(result);
+}
+
+export async function compress(bytes) {
+	return await applyStream(bytes, new CompressionStream('gzip'));
+}
+
+export async function decompress(bytes) {
+	return await applyStream(bytes, new DecompressionStream('gzip'));
+}
+
 export async function getKey(password) {
 	var encoder = new TextEncoder();
 	var encoded = encoder.encode(password);
@@ -26,4 +80,20 @@ export async function decrypt(bytes, key) {
 	return new Uint8Array(await crypto.subtle.decrypt(
 		{name: 'AES-GCM', iv: iv}, key, encrypted
 	));
+}
+
+export async function encryptText(text, key) {
+	var encoder = new TextEncoder();
+	var bytes = encoder.encode(text);
+	bytes = await compress(bytes);
+	bytes = await encrypt(bytes, key);
+	return b64encode(bytes);
+}
+
+export async function decryptText(string, key) {
+	var decoder = new TextDecoder();
+	var bytes = b64decode(string);
+	bytes = await decrypt(bytes, key);
+	bytes = await decompress(bytes);
+	return decoder.decode(bytes);
 }
--- static/main.js
+++ static/main.js
@@ -1,31 +1,13 @@
-import * as base64 from './base64.js';
 import * as crypto from './crypto.js';
-import * as gzip from './gzip.js';
 
 var password = window.prompt('Please enter the password');
 var key = await crypto.getKey(password);
-
-var encode = async function(text) {
-	var encoder = new TextEncoder();
-	var bytes = encoder.encode(text);
-	bytes = await gzip.compress(bytes);
-	bytes = await crypto.encrypt(bytes, key);
-	return base64.encode(bytes);
-};
-
-var decode = async function(string) {
-	var decoder = new TextDecoder();
-	var bytes = base64.decode(string);
-	bytes = await crypto.decrypt(bytes, key);
-	bytes = await gzip.decompress(bytes);
-	return decoder.decode(bytes);
-};
 
 var textarea = document.querySelector('textarea');
 
 if (location.hash.length) {
 	try {
-		textarea.value = await decode(location.hash.substr(1));
+		textarea.value = await crypto.decryptText(location.hash.substr(1), key);
 	} catch (e) {
 		window.confirm('Decryption failed');
 		console.exception(e);
@@ -33,6 +15,6 @@ if (location.hash.length) {
 }
 
 textarea.addEventListener('input', async () => {
-	var data = await encode(textarea.value);
+	var data = await crypto.encryptText(textarea.value, key);
 	history.replaceState(null, '', `#${data}`);
 });
